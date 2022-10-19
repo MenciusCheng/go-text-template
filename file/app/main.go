e2324fceb7c86b6ec17026cff1ec39868f392b95
chengmengwei <chengmengwei@inke.cn>
2022-10-19 17:19:10 +0800

    文件处理

---
+++ file/app/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/MenciusCheng/go-text-template/parse"
+	"io/fs"
+	"io/ioutil"
+)
+
+func main() {
+	textStr, err := ioutil.ReadFile("../json/hello.json")
+	if err != nil {
+		panic(err)
+	}
+
+	textMap, err := parse.StringToMap(string(textStr))
+	if err != nil {
+		panic(err)
+	}
+
+	tmplStr, err := ioutil.ReadFile("../tmpl/hello.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := parse.JsonMap(string(tmplStr), textMap)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("../out/hello.txt", []byte(res), fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
