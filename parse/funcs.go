e5c676b17fb179b2ec86a1c41a7c32401b9565d7
chengmengwei <chengmengwei@inke.cn>
2022-10-27 12:17:33 +0800

    更新模版

--- parse/funcs.go
+++ parse/funcs.go
@@ -5,6 +5,7 @@ import "text/template"
 func GetFuncMap() template.FuncMap {
 	return template.FuncMap{
 		"IsInStringSlice": IsInStringSlice,
+		"IfElse":          IfElse,
 	}
 }
 
@@ -15,4 +16,11 @@ func IsInStringSlice(el string, arr []interface{}) bool {
 		}
 	}
 	return false
+}
+
+func IfElse(a bool, b, c interface{}) interface{} {
+	if a {
+		return b
+	}
+	return c
 }
