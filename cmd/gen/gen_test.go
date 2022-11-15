a24e53ec4ee4e9c68dd0cc503593e5431ca237bb
chengmengwei <chengmengwei@inke.cn>
2022-11-15 14:58:21 +0800

    更新模版

--- cmd/gen/gen_test.go
+++ cmd/gen/gen_test.go
@@ -2,7 +2,27 @@ package gen
 
 import "testing"
 
-func TestGenByDirConfig(t *testing.T) {
+func TestGenByDirConfig_Router(t *testing.T) {
+	type args struct {
+		dirPath string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{args: args{dirPath: "../config/router"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := GenByDirConfig(tt.args.dirPath); (err != nil) != tt.wantErr {
+				t.Errorf("GenByDirConfig() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGenByDirConfig_Crud(t *testing.T) {
 	type args struct {
 		dirPath string
 	}
@@ -12,7 +32,6 @@ func TestGenByDirConfig(t *testing.T) {
 		wantErr bool
 	}{
 		{args: args{dirPath: "../config/crud_dao"}},
-		//{args: args{dirPath: "../config/router"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
